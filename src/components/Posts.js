d92e5f20f646573a9755badfbf95f35e4d6fd64a
Natalia <natalia_vileyto@outlook.com>
2019-10-31 23:06:21 +0300

    create error

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,41 +1,81 @@
-import React, {Component} from 'react';
-import InstaServices from '../services/instaService.js';
+import React, { Component } from 'react';
+import User from './User';
+import ErrorMessage from './Error';
+import InstaService from '../services/instaService';
+
+class Posts extends Component {
+
+  InstaService = new InstaService();
+
+  state = {
+    posts: [],
+    error: false
+  }
+
+  componentDidMount() {
+    this.updatePosts();
+  }
+
+  updatePosts() {
+    this.InstaService.getAllPosts()
+      .then(this.onPostsLoaded)
+      .catch(this.onError);
+  }
+
+  onPostsLoaded = (posts) => {
+    this.setState({
+      posts: posts, // es6 posts,
+      error: false
+    })
+  }
+
+  onError = (err) => {
+    this.setState({
+      error: true
+    })
+  }
+
+  renderItems(array) {
+    return array.map(item => {
+      const { name, altname, photo, src, alt, descr, id } = item;
 
 
-export default class Posts extends Component {
 
-    InstaService = new InstaServices();
-    state = {
-        posts: [],
-        error: false
+      return (
+        <div key={id} className="post">
+          <User
+            src={photo}
+            alt={altname}
+            name={name}
+            min
+          />
+          <img src={src} alt={alt}></img>
+          <div className="post__name">
+            {name}
+          </div>
+          <div className="post__descr">
+            {descr}
+          </div>
+        </div>
+      )
+    });
+  }
+
+  render() {
+    const { error, posts } = this.state;
+
+    if (error) {
+      return <ErrorMessage />
     }
 
-    componentDidMount() {
-        this.updatePosts();
-    }
+    const items = this.renderItems(posts);
 
-    updatePosts() {
-        this.InstaService.getAllPosts().then(this.onPostsLoaded).catch(this.onError);
-    }
-
-    onPostsLoaded = (posts) => {
-        this.setState({
-            posts,
-            error: false
-        }); 
-    }
-
-    onError = (err) => {
-        this.setState({
-            error: true
-        }); 
-    }
-
-    render () {
-        return (
-            <div className="left">
-                 
-            </div>
-        )
-    }
+    return (
+      <div className="left">
+        {items}
+      </div>
+    )
+  }
 }
+
+export default Posts;
--- src/services/instaService.js
+++ src/services/instaService.js
@@ -1,20 +1,32 @@
-export default class InstaService {
+export default class instaService {
     constructor() {
-        this._apiBase = 'http://localhost:3000';
+      this._apiBase = 'http://localhost:3000';
     }
-
+  
     getResource = async (url) => {
-        const res = await fetch(`${this._apiBase}${url}`);
-
-        if(!res.ok){
-            throw new Error(`Could not fetch ${url}; recived ${res.status}`)
-        }
-
-        return await res.json();
+      const response = await fetch(`${this._apiBase}${url}`);
+  
+      if (!response.ok) {
+        throw new Error(`Нет ответа ${url}; получили ошибку ${response.status}`);
+      }
+  
+      return await response.json();
     }
-
+  
     getAllPosts = async () => {
-        const res = await this.getResource('/posts/').then;
-        return res;
+      const response = await this.getResource('/posts/');
+      return response;
     }
-}
+  
+    getAllPhotos = async () => {
+      const response = await this.getResource('/posts/');
+      return response.map(this._transformPost);
+    }
+  
+    _transformPost = (post) => {
+      return {
+        src: post.src,
+        alt: post.alt
+      }
+    }
+  }
